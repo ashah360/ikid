780261e3cfb07d0db6c5bcf5225f1307668528a9
skarahx <arman.shah0700@gmail.comgit>
2018-10-24 23:00:58 -0700

    first

---
+++ ikid/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ikid
+//
+//  Created by Arman Shah on 10/24/18.
+//  Copyright © 2018 Arman Shah. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
